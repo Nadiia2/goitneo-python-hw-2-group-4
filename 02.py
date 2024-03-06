7aca62775dd79b61a9a097be11fb561cc142d1fa
Nadiia2 <149429423+Nadiia2@users.noreply.github.com>
2024-03-06 18:05:10 +0100

    Initial commit

---
+++ 02.py
@@ -0,0 +1,50 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+def change_contacts(args, contacts):
+    name, new_phone = args
+    for key in contacts.keys():
+        if key == name:
+            contacts[key] = new_phone
+    return "Contact updated."
+
+def show_phone(args, contacts):
+    name = args[0]
+    return contacts.get(name)
+
+def show_all(contacts):
+    return contacts.items()
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contacts(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            print(show_all(contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
